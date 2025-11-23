55ff61292060ff6271ecf91d60968637ada02c96
Bijeesh <bijeeshb.stackup@gmail.com>
2025-11-24 00:43:12 +0530

    redirection bug fix

--- src/app/nearpay/page.js
+++ src/app/nearpay/page.js
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-export default function Home() {
+export default function NearpayPage() {
   
-  redirect("/dashboard");
+  redirect("/nearpay/dashboard");
 }
